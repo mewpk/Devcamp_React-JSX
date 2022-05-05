0fa25cd462ff282d7cdc2574afaf9d1efbdd21cf
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-05 23:07:18 +0700

    save

---
+++ react-jsx1.2/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./Component/Header"
+import Content from "./Component/Content"
+import Footer from "./Component/Footer"
+import './App.css';
+import React from "react"
+class App extends React.Component {
+  render () {
+  return <>
+  <h1>Hello</h1>
+  <Header/>
+  <Content/>
+  <Footer/>
+  </>
+  }
+ }
+ export default App;
+ 
