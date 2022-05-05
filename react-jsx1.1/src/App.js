44a08b51416a4a67ffa004abcbcf8d3b2e250300
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-05 23:02:01 +0700

    save

---
+++ react-jsx1.1/src/App.js
@@ -0,0 +1,18 @@
+import Header from "./component/Header"
+import Content from "./component/Content"
+import Footer from "./component/Footer"
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+   <h1>Hello</h1>
+   <Header/>
+   <Content/>
+   <Footer/>
+   </>
+  );
+}
+
+export default App;
---
+++ react-jsx1.1/src/component/Content.js
@@ -0,0 +1,5 @@
+const Content = () => {
+    return <h1>Content</h1>
+}
+
+export default Content
