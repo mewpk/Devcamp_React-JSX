fee3530a710882bc5efa9a5e2adf44a05e8222dd
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-06 15:13:41 +0700

    save

---
+++ pikkanode-react/src/Components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Style.css"
+export default function Navbar() {
+  return (
+    <div>
+        
+      <ul className='Navbar'>
+  
+        <li><img src="https://raw.githubusercontent.com/panotza/pikkanode/master/pikkanode.png" height={50} weight={50} alt="My Awesome Image"/></li>
+        <li>Create pikka</li>
+        <li>signup</li>
+        <li>signin</li>
+        <li>signout</li>
+      </ul>
+    </div>
+
+  )
+}
